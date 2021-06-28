aca69267192841b9cc7796361f56b25a24af2dc1
brk-a <fnjakai@outlook.com>
2021-06-28 04:09:26 -0700

    README, holberton.h, Tasks 0 - 9 Try 1.0

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int i, j;
+
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 != 0)
+{
+j = i / 2;
+}
+else
+{
+j = (i - 1) / 2;
+}
+while (j <= i)
+{
+_putchar(str[j]);
+j++;
+}
+_putchar(10);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+#0x05 Pointers abd Arrays: README
