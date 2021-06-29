a2d21ac861a04450f734a5ec2a23d4ce5fe54947
brk-a <fnjakai@outlook.com>
2021-06-29 07:00:42 -0700

    try 6.1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,16 +12,16 @@ int num = 0;
 char neg = 0;
 char allow_any = 1;
 
-while (*s != 0 && (allow_any || (*s >= 48 && *s <= 57)))
+while (*s != '\0' && (allow_any || (*s >= '0' && *s <= '9')))
 {
-if (*s >= 48 && *s <= 57)
+if (*s >= '0' && *s <= '9')
 {
 allow_any = 0;
-num = num * 10 (neg ? -(*s - '0') : (*s - '0'));
+num = num * 10 + (neg ? -(*s - '0') : (*s - '0'));
 }
 else if (*s == '-')
 {
-neg = ! neg;
+neg = !neg;
 s++;
 }
 }
