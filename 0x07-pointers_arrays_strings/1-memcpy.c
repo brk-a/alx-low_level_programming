bf13c6ebf8c9fabb781ba81a58ba984cc35730b9
brk-a <fnjakai@outlook.com>
2021-07-05 07:54:56 -0700

    Try v 1.0, README and holberton.h

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+while(i == 0, i < n)
+{
+dest[i] = src[i];
+i++;
+}
+
+return (dest);
+}
