582e437efccb8b1d79b87574a8f47e7cb7aa36b5
brk-a <fnjakai@outlook.com>
2021-07-07 01:04:49 -0700

    README, holberton.h and Tasks 0 - 6, try v 1.0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * check_int - checks integer as candidate for square root
+ * @square: square integer
+ * @x: integer
+ * Return: int if success and -1 if fail
+ */
+
+int check_int(int square, int x)
+{
+if (x * x == square)
+return (x);
+else if (x > square / x)
+return (check_int(square, x - 1));
+else if (x < square / x)
+return (check_int(square, x + 1));
+else
+return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ * Return: integer - square root, if none returns -1
+ */
+
+int _sqrt_recursion(int n)
+{
+int start;
+
+start = 0;
+
+if (n < 0)
+return (-1);
+else if (n == 0)
+return (0);
+else
+return (check_int(n, start));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+include "holberton.h"
+
+/**
+* is_prime_number - see if input is a prime number
+* @n: input
+*
+*
+*Return: 1 if prime, 0 otherwise
+*/
+
+int is_prime_number(int n)
+{
+
+int i = 2;
+
+if (n <= 2)
+return ((n == 2) ? 1 : 0);
+if (n % i == 0)
+return (0);
+if (i * i > n)
+return (1);
+
+i += 1;
+return (is_prime_nubmer(n));
+}
