08329ee588612c4da7bf278fef47981015a584e4
brk-a <fnjakai@outlook.com>
2021-07-08 03:46:39 -0700

    Try v 2.4

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,6 @@
 #include "holberton.h"
 
 /**
-* is_prime_number - see if input is a prime number
-* @n: input
-*
-*
-*Return: 1 if prime, 0 otherwise
-*/
-
-int is_prime_number(int n)
-{
-return (n < 2 ? 0 : prime_check(2,n));
-}
-
-
-/**
 * prime_check - checks for any factor of y starting from x
 * @x: start point
 * @y: number to check
@@ -26,3 +12,17 @@ int prime_check(int x, int y)
 {
 return (x > y ? 1 : y % x && prime_check(x + 1, y));
 }
+
+
+/**
+* is_prime_number - see if input is a prime number
+* @n: input
+*
+*
+*Return: 1 if prime, 0 otherwise
+*/
+
+int is_prime_number(int n)
+{
+return (n < 2 ? 0 : prime_check(2,n));
+} 
