a7131b1b53d6e16c79c6fe87c130f23ddd16ac52
brk-a <fnjakai@outlook.com>
2021-07-08 02:49:27 -0700

    Try v 2.0

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+*wildcmp - checks whether 2 strings match
+*@s1: a string
+*@s2: a string pattern
+*
+*
+*Return: 1 if match, 0 otherwise
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+if (*s2 == '*')
+{
+if(s1 == '\0' || *(s2 + 1) == '*')
+{
+return (wildcmp(s1, s2 + 1));
+}
+else if (wildcmp(s1, s2 + 1))
+{
+return (1);
+}
+return (wildcmp(s1 + 1, s2));
+}
+else if (*s2 == '\0')
+{
+return (*s1 == '\0');
+}
+return (*s1 == *s2 && wildcmp(s1 + 1, s2 + 1));
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,9 +16,9 @@ int i = 2;
 if (n <= 2)
 return ((n == 2) ? 1 : 0);
 if (n % i == 0)
-return (0);
-if (i * i > n)
 return (1);
+if (i * i > n)
+return (0);
 
 i += 1;
 return (is_prime_number(n));
