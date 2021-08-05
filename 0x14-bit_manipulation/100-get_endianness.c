cd7a63f9dcbca4a946e286b84f33cb540450ac40
brk-a <fnjakai@outlook.com>
2021-08-05 05:45:13 -0700

    Task 6: try v 2.0: changes to variable wee_or_big

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * get_endianness - Checks the systems Byte order
+ * get_endianness - Checks the sys byte order
  * Return: Returns 0 if big endian 1 if lil endian
  */
 int get_endianness(void)
 {
 int x;
-int little_or_big;
+int wee_or_big;
 
 x = 1;
-lil_or_big = (int) (((char *)&x)[0]);
-return (lil_or_big);
+wee_or_big = (int) (((char *)&x)[0]);
+return (wee_or_big);
 }
