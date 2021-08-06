40f4087f5c875092329d000b540ccf7a62f2a553
brk-a <fnjakai@outlook.com>
2021-08-06 11:53:58 -0700

    Task 3: try v 4.0; new algo

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,28 +1,18 @@
 #include "holberton.h"
 
 /**
-* _strcmp - function that...
-*@s1: input string
-* @s2: string to compare with
-* Return: 0 if no match, 1 otherwise
-*/
-
+ * _strcmp - compares s1 with s2
+ * @s1: a string
+ * @s2: a string
+ *
+ * Return: difference of the first non-matching character pair
+ */
 int _strcmp(char *s1, char *s2)
 {
+int diff = 0;
 
-int i = 0, arr[1000] = {0}, *ptr, sum = 0;
-ptr = &sum;
+while (*s1 && *s2 && !(diff = *s1++ - *s2++))
+continue;
 
-while (*(s2 + i) != '\0')
-{
-arr[i] = (*(s1 + i) == *(s2 + i)) ? 1 : 0;
-i++;
-}
-
-for (; i >= 0; i--)
-{
-*ptr += arr[i];
-}
-
-return ((*ptr == 0) ? 0 : 1);
+return (diff);
 }
