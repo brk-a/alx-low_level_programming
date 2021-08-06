ef17f8cea52fb31dd9e34c80ae767b36bb887b15
brk-a <fnjakai@outlook.com>
2021-08-06 12:15:59 -0700

    Task 9: try v 4.0; new algo

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,44 +1,28 @@
 #include "holberton.h"
-#include <math.h>
-
-int printDigit(int N);
 
 /**
-*print_number: print out an integer
-*printDigit : print an int
-*@n:integer to print
-*
-*Return: void
-*/
-
+ * print_number - prints an integer
+ * @n: number to print
+ *
+ * Return: void
+ */
 void print_number(int n)
 {
+char digit, significant_zero = 0;
+int divisor = 1000000000;
 
-/* int i = 0, j; */
-/*i = (n == 0) ? 1  : (log10(n) + 1);*/
-/* for (j = 0; j < i; j++)*/
-
-int r;
-
-if (n == 0)
+if (n < 0)
+_putchar('-');
+while (divisor != 0)
 {
-_putchar(48);
-}
-else
-{
-printDigit(n);
-_putchar('0' + r);
-}
+digit = n / divisor;
+digit = digit < 0 ? -digit : digit;
+n = n % divisor;
+divisor /= 10;
 
+significant_zero = significant_zero || digit != 0 || divisor == 0;
+if (!digit && !significant_zero)
+continue;
+_putchar(digit + '0');
 }
-
-int printDigit(int N)
-{
-int r;
-if (N == 0)
-{
-return (NULL);
-}
-r = N % 10;
-return (printDigit(N / 10));
 }
