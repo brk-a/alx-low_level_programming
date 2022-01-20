4e117fcb1d5639b9f08504490fd41ae4efcd726a
brk-a <fnjakai@outlook.com>
2022-01-19 22:40:00 -0800

    task0: try v 2.0; betty checks :(

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,12 +1,12 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a val in arr of int w/linear search algo
- * @array: ptr to arr of ints
- * @size: size of arr
- * @value: val to find
- * Return: index of first match
- */
+* linear_search - searches for a val in arr of int w/linear search algo
+* @array: ptr to arr of ints
+* @size: size of arr
+* @value: val to find
+* Return: index of first match
+*/
 
 int linear_search(int *array, size_t size, int value)
 {
