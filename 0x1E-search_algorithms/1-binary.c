403c7c2f4e6532f685c4e5b6a08cd22de113ea39
brk-a <fnjakai@outlook.com>
2022-01-21 04:00:07 -0800

    Task 1: try v 3.0; more betty checks :(

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,15 +8,15 @@
  */
 
 void print_subarray(int *array, size_t first, size_t last)
-{                                                                                                                   
-  size_t i = 0;                                                                                               
-                                                                                                                    
-  printf("Searching in array: ");                                                                             
-  for (i = first; i < last; i++)                                                                              
-    {                                                                                                           
-      printf("%d, ", array[i]);                                                                           
-    }                                                                                                           
-  printf("%d\n", array[i]);                                                                                   
+{
+size_t i = 0;
+
+printf("Searching in array: ");
+for (i = first; i < last; i++)
+{
+printf("%d, ", array[i]);
+}
+printf("%d\n", array[i]);
 }
 
 /**
@@ -29,25 +29,25 @@ void print_subarray(int *array, size_t first, size_t last)
 
 int binary_search(int *array, size_t size, int value)
 {
-  size_t first = 0;
-  size_t last = 0;
-  size_t mid = 0;
+size_t first = 0;
+size_t last = 0;
+size_t mid = 0;
 
-  if (array == NULL || size == 0)
-    return (-1);
+if (array == NULL || size == 0)
+return (-1);
 
-  last = size - 1;
+last = size - 1;
 
-  while (first <= last)
-    {
-      print_subarray(array, first, last);
-      mid = (first + last) / 2;
-      if (array[mid] > value)
-	last = mid - 1;
-      else if (array[mid] < value)
-	first = mid + 1;
-      else
-	return (mid);
-    }
-  return (-1);
+while (first <= last)
+{
+print_subarray(array, first, last);
+mid = (first + last) / 2;
+if (array[mid] > value)
+last = mid - 1;
+else if (array[mid] < value)
+first = mid + 1;
+else
+return (mid);
+}
+return (-1);
 }
